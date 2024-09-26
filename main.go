e6730f21a294c02a30835220a093c3eda535a101
Calebbuffleben <calebbuffleben@gmail.com>
2024-09-26 11:15:40 -0300

    feat: logger

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "github.com/Calebbuffleben/api-with-golang/router"
+import (
+	"github.com/Calebbuffleben/api-with-golang/router"
+	"github.com/Calebbuffleben/api-with-golang/config"
+)
 
 func main() {
+
+	err := config.Init()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 	router.Initialize()
 }
