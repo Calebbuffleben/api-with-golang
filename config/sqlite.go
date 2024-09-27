e7704850cc5a21c5fd5f7df7b69d4b1b11a3b56a
Calebbuffleben <calebbuffleben@gmail.com>
2024-09-27 17:03:56 -0300

    loger file

--- config/sqlite.go
+++ config/sqlite.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"github.com/Calebbuffleben/api-with-golang/schemas"
 	"gorm.io/gorm"
 	"gorm.io/driver/sqlite"
 )
@@ -9,4 +10,17 @@ func InitializeSQLite() (*gorm.DB, error){
 	logger := GetLogger("sqlite")
 
 	db, err := gorm.Open(sqlite.Open("./db/openings.db"), &gorm.Config{})
+
+	if err != nil {
+		logger.Errorf("sqlite opening error: %v", err)
+		return nil, err
+	}
+
+	err = db.AutoMigrate(&schemas.Opening{})
+	if err != nil {
+		logger.Errorf("sqlite auto migration: %v", err)
+		return nil, err
+	}
+
+	return db, nil
 }
