9c5097ca74102a430aa4b510b9b3e1c03834bb5b
Calebbuffleben <calebbuffleben@gmail.com>
2024-10-07 11:11:13 -0300

    creating database

--- handler/createOpening.go
+++ handler/createOpening.go
@@ -1,6 +1,10 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+	"github.com/Calebbuffleben/api-with-golang/schemas"
+)
 
 func CreateOpeningHandler(ctx *gin.Context) {
 	request := CreateOpeningRequest{}
@@ -9,11 +13,24 @@ func CreateOpeningHandler(ctx *gin.Context) {
 
 	if err := request.Validate(); err != nil{
 		logger.Errorf("validation error %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
 		return
 	}
 
-	if err := db.Create(&request).Error; err!= nil{
+	opening := schemas.Opening{
+		Role:     request.Role,
+		Company:  request.Company,
+		Location: request.Location,
+		Remote:   *request.Remote,
+		Link:     request.Link,
+		Salary:   request.Salary,
+	}
+
+	if err := db.Create(&opening).Error; err!= nil{
 		logger.Errorf("error creating opening: %v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, "error creating opening on database")
 		return
 	}
+
+	sendSuccess(ctx, "create-opening", opening)
 }
