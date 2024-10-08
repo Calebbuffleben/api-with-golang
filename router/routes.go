f2316518b82b7b7f6b3941f6ee0477ad23a726f4
Calebbuffleben <calebbuffleben@gmail.com>
2024-10-08 17:17:31 -0300

    feat: swagger

--- router/routes.go
+++ router/routes.go
@@ -3,11 +3,16 @@ package router
 import (
 	"github.com/Calebbuffleben/api-with-golang/handler"             
 	"github.com/gin-gonic/gin"
+	"github.com/Calebbuffleben/api-with-golang/docs"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func initializeRoutes(router *gin.Engine) {
 	handler.InitializeHandler()
-	v1 := router.Group("/api/v1")
+	basePath := "/api/v1"
+	docs.SwaggerInfo.BasePath = basePath
+	v1 := router.Group(basePath)
 	{
 		v1.GET( "/opening", handler.ShowOpeningHandler)
 		v1.POST( "/opening", handler.CreateOpeningHandler)
@@ -15,4 +20,6 @@ func initializeRoutes(router *gin.Engine) {
 		v1.PUT( "/opening", handler.UpdateOpeningHandler)
 		v1.GET( "/openings", handler.ListOpeningHandler)
 	}
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 }
